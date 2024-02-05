4d3c08221e87971e8ae832726ba31d636a46bfac
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-05 11:32:20 -0300

    Update inputs: uncontrolled to controlled

--- src/_layouts/auth/auth.tsx
+++ src/_layouts/auth/auth.tsx
@@ -1,22 +1,62 @@
 import { Bag, SignIn } from "phosphor-react";
 import { CounterBG, Container, FormContainer, Wrapper, InputEmail, InputPassword, ButtonSubmit } from "./style";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import * as z from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+
+const loginFormSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
+
+type loginFormSchemaInputs = z.infer<typeof loginFormSchema>
 
 export function Auth(){
 
+    const navigate = useNavigate()
 
+    const { 
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+        reset,
+    } = useForm<loginFormSchemaInputs>({
+        resolver: zodResolver(loginFormSchema)
+    })
+
+    function handleLogin(data: loginFormSchemaInputs){
+        console.log(data.email)
+        console.log(data.password)
+        reset()
+        navigate('/')
+
+    }
 
     return(
     <>
     <Container>
     <CounterBG/>
-            <FormContainer>
+            <FormContainer action="" onSubmit={handleSubmit(handleLogin)}>
                 <h1>Welc<Bag/>me back!</h1>
                 <Wrapper>
-                <InputEmail type="email" placeholder="Login" />
-                <InputPassword type="password" placeholder="Password"/>
-                <ButtonSubmit><SignIn size={28}/></ButtonSubmit>
+
+                <InputEmail
+                 type="email" placeholder="Email" 
+                 {...register('email')}
+                 required
+                 />
+
+                <InputPassword 
+                type="password" placeholder="Password"
+                {...register('password')}
+                required
+                />
+
+                <ButtonSubmit type="submit" disabled={isSubmitting}>
+                    <SignIn size={28}/>
+                </ButtonSubmit>
                 </Wrapper>
                 <span>register <Link to="/register" className="link">here</Link></span>
             </FormContainer>
