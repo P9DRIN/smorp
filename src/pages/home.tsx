2afb422b8edb6f04f61c5be60333fdb47d65f6a0
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-05 11:36:39 -0300

    update: loading function added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,16 +2,22 @@ import { useContext } from "react";
 import { Card } from "../components/Card";
 import { Container } from './homestyle'
 import { ProductContext } from "../context/ProductContext";
+import { Loading } from "../components/Loading";
 
 export function Home(){
     
     const { product } = useContext(ProductContext)
 
-
     return(
         <>
         <Container>
-            {product.map((prod) => <Card key={prod.id} data={prod} />)}
+            
+            {
+            product.length ?
+            product.map((prod) => <Card key={prod._id} data={prod} />)
+            :
+            <Loading/>
+            }
         </Container>
         </>
     )
