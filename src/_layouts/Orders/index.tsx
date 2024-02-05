d4630185a01e4c7167e50d40bcae625ddbfcea94
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-05 11:56:28 -0300

    add: started orders & profile page

---
+++ src/_layouts/Orders/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+export function OrdersPage(){
+
+    return(
+        <>
+        <div>
+        <Link to="/"><h1>orders</h1></Link>
+        </div>
+        </>
+
+    )
+}
---
+++ src/_layouts/profile/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+export function ProfilePage(){
+
+    return(
+        <>
+        <div>
+        <Link to="/"><h1>profile</h1></Link>
+        </div>
+        </>
+    )
+
+}
