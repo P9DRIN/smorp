c17751ab08f840123da75ab8fe98097b87e86c32
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 15:30:51 -0300

    add responsive success buy page

--- src/_layouts/success/index.tsx
+++ src/_layouts/success/index.tsx
@@ -1,10 +1,42 @@
+import { Link } from "react-router-dom";
+import { Container, Header, Logo } from "./style";
+import { Bag, CaretRight, Check } from "phosphor-react";
+import { DropDown } from "../appLayout/components/Dropdown/Dropdown";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 
 export function Success(){
 
+    const { info } = useContext(AuthContext)
+
     return(
         <>
-        <h1>sucesso</h1>
+        <Header>
+            <Link to="/">
+            <Logo>
+                <span>S</span>
+                <Bag size={26} weight="bold"/>
+                <span>ORP!</span>
+            </Logo>
+            </Link>
+            <DropDown/>
+        </Header>
+        <Container>
+        <div className="transitionWrapper">
+        <div className="circle"><Check weight="bold"/></div>
+        </div>
+        <h1 className="success">SUCESSO!</h1>
+        <span className="confirmedOrderTag">Pedido<b> confirmado.</b></span>
+        <div className="infoContainer">
+            <span className="fullName">{info?.fullName},<br/></span>
+            <span>Neste momento, <b>seu pedido</b> está sendo preparado!<br/> Logo estará a caminho de você!</span>
+        </div>
+        <Link to="/orders">
+            <span className="orders">Ver <b> meus pedidos... </b> <CaretRight size={24}/></span>
+        </Link>
+        </Container>
+
         </>
     )
 }
--- src/_layouts/success/style.ts
+++ src/_layouts/success/style.ts
@@ -1,3 +1,151 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Container = styled.div``
+
+export const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-content: space-around;
+    background: #111e29;
+`
+
+export const Logo = styled.div`
+    color: ${props => props.theme['blue']};
+    font-weight: bold;
+    font-size: 24px;
+    display: flex;
+    justify-content: center;
+
+`
+export const Container = styled.div`
+    @keyframes circle{
+        0%{
+            border-top-color: ${props => props.theme['blue-100']};
+            border-right-color: ${props => props.theme['blue-300']};
+            border-bottom-color: ${props => props.theme['blue-300']};
+            border-left-color: ${props => props.theme['blue-300']};
+        }
+        33%{
+            border-top-color: ${props => props.theme['blue-100']};
+            border-right-color: ${props => props.theme['blue-100']};
+            border-bottom-color: ${props => props.theme['blue-300']};
+            border-left-color: ${props => props.theme['blue-300']};
+        }
+        66%{
+            border-top-color: ${props => props.theme['blue-100']};
+            border-right-color: ${props => props.theme['blue-100']};
+            border-bottom-color: ${props => props.theme['blue-100']};
+            border-left-color: ${props => props.theme['blue-300']};
+        }
+        80%{
+            border-top-color: ${props => props.theme['blue-100']};
+            border-right-color: ${props => props.theme['blue-100']};
+            border-bottom-color: ${props => props.theme['blue-100']};
+            border-left-color: ${props => props.theme['blue-100']};
+        }
+        100%{
+            border-top-color: ${props => props.theme['blue']};
+            border-right-color: ${props => props.theme['blue']};
+            border-bottom-color: ${props => props.theme['blue']};
+            border-left-color: ${props => props.theme['blue']};
+            box-shadow: -1px 0px 20px 10px rgba(0,166,251,0.2);
+        }
+    }
+
+    @keyframes growthUp{
+        0%{
+        font-size: 60px;
+        stroke-dashoffset: 251.2;
+        transform: rotate(400deg);
+        color: ${props => props.theme['white-soft']};
+        }
+        25%{
+        transform: rotate(220deg);
+
+        }
+        50%{
+        transform: rotate(380deg);
+
+        }
+        100%{
+        font-size: 80px;
+        stroke-dashoffset: 0;
+        transform: rotate(360deg);
+        color: ${props => props.theme['white']};
+        }
+    }
+    
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    & .transitionWrapper{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+        padding: 0.5rem;
+        border-radius: 50%;
+        border: 10px solid ${props => props.theme['blue']};
+        animation: circle 1.75s step-start;
+        margin-top: 8rem;
+        box-shadow: -1px 0px 20px 14px rgba(0,166,251,0.4);
+    }
+    & svg{
+        font-size: 80px;
+        animation: growthUp 1.90s forwards;
+        stroke-dasharray: 251.2;
+        stroke-dashoffset: 0;
+    }
+
+    & .success{
+        margin-top: 1.25rem;
+        font-size: 40px;
+        color: ${props => props.theme['white']};
+        font-weight: 400;
+    }
+
+    & .confirmedOrderTag{
+        color: ${props => props.theme['gray']};
+
+        & b{
+            color: ${props => props.theme['blue']};
+        }
+    }
+    & .infoContainer{
+        margin-top: 1rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+
+        & .fullName{
+            color: ${props => props.theme['blue']};
+            font-weight: bold;
+        }
+        & span{
+            text-align: center;
+        }
+    }
+
+    & .orders{
+        margin-top: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        gap: 0.2rem;
+        color: ${props => props.theme['blue']};
+
+        & svg{
+        margin-left: -5px;
+        color: ${props => props.theme['blue']} !important;
+
+        }
+
+    }
+
+`
