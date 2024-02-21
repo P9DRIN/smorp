da84b990b17fcf329467417ce010b7f57108fba2
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 15:16:12 -0300

    add private routes into routes app

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,49 +1,64 @@
-import { createBrowserRouter } from "react-router-dom";
 import { AppLayout } from "./_layouts/appLayout/app";
 import { Auth } from "./_layouts/auth/auth";
-import { Home } from "./pages/home";
 import { Register } from "./_layouts/register/register";
 import { CartPage } from "./_layouts/cart";
 import { Success } from "./_layouts/success";
 import { PaymentPage } from "./_layouts/payment";
-import { ProfilePage } from "./_layouts/profile";
+import { ProfilePage } from "./_layouts/register/profile";
 import { OrdersPage } from "./_layouts/Orders";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Fragment, ReactNode, useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import { Loading } from "./components/Loading";
+import { LoaderContainer } from "./components/Loading/style";
+import { OrderPage } from "./_layouts/Order";
+
+interface RouterProps {
+    children: ReactNode
+}
 
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <AppLayout/>,
-        children: [{
-            path: '/', element: <Home/>
-        }],
-    },
-    {
-        path: '/auth',
-        element: <Auth/>
-    },
-    {
-        path: '/register',
-        element: <Register/>
-    },
-    {
-        path: '/cart',
-        element: <CartPage/>
-    },
-    {
-        path: '/payment',
-        element: <PaymentPage/>
-    },
-    {
-        path: '/profile',
-        element: <ProfilePage/>
-    },
-    {
-        path: '/orders',
-        element: <OrdersPage/>
-    },
-    {
-        path: '/success',
-        element: <Success/>
-    },
-])
+const Private = ({ children }: RouterProps) => {
+
+    const { Authenticated, loading } = useContext(AuthContext);
+
+    if(loading){
+        return(
+            <>
+            <LoaderContainer/>
+            <Loading/>
+            </>
+            )
+    }
+    if(!Authenticated){
+        return <Navigate to='/auth'/>
+    }
+    return children;
+}
+
+
+const RoutesApp = () => {
+    return(
+        <Fragment>
+
+            <Routes>
+
+                <Route path="/" element={<AppLayout/>}/>
+                <Route path="/auth" element={<Auth/>}/>
+                <Route path="/register" element={<Register/>}/>
+
+                <Route path="/cart" element={<Private><CartPage/></Private>}/>
+                <Route path="/payment" element={<Private><PaymentPage/></Private>}/>
+                <Route path="/profile" element={<Private><ProfilePage/></Private>}/>
+                <Route path="/orders" element={<Private><OrdersPage/></Private>}>
+                </Route>
+                    <Route path='orders/:id' element={<OrderPage/>}/>
+                <Route path="/success" element={<Private><Success/></Private>}/>
+
+            </Routes>
+        </Fragment>
+    )
+}
+
+export default RoutesApp;
+
