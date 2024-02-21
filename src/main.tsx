b3c79c15802ea0d00710c53c67e92d010908208e
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 15:15:56 -0300

    add private routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { ProductProvider } from './context/ProductContext.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { OrderProvider } from './context/OrderContext.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'; 
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Router>
+    <AuthProvider>
+      <OrderProvider>
     <ProductProvider>
-    <App />
+      <App />
     </ProductProvider>
+      </OrderProvider>
+    </AuthProvider>
+    </Router>
+
   </React.StrictMode>,
 )
