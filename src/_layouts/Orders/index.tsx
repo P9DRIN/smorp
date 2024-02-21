df0d08f26b7dc926adc50dc67cd09a982b784f5e
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 15:26:17 -0300

    add link w/ API and display orders info

--- src/_layouts/Orders/index.tsx
+++ src/_layouts/Orders/index.tsx
@@ -1,14 +1,47 @@
 import { Link } from "react-router-dom";
+import { Container, Header, Logo, OrderList } from "./style";
+import { Bag } from "phosphor-react";
+import { DropDown } from "../appLayout/components/Dropdown/Dropdown";
+import { OrderComponent } from "./OrderComponent";
+import { OrderContext } from "../../context/OrderContext";
+import { Loading } from "../../components/Loading";
+import { useContext, useEffect } from "react";
 
 
 export function OrdersPage(){
 
+    const { order, fetchOrders } = useContext(OrderContext)
+
+    useEffect(() => {
+        fetchOrders()
+    }, [])
+
     return(
         <>
-        <div>
-        <Link to="/"><h1>orders</h1></Link>
-        </div>
-        </>
+        <Header>
+            <Link to="/">
+            <Logo>
+                <span>S</span>
+                <Bag size={26} weight="bold"/>
+                <span>ORP!</span>
+            </Logo>
+            </Link>
+            <DropDown/>
+        </Header>
+        <Container>
+            <OrderList>
+            {
+            order.length ?
+            order.map((ord) => <OrderComponent key={ord.orderNum} data={ord} />)
+            :
+            <div className="mid">
+            <Loading/>
+            </div>
+            }
+        
+            </OrderList>
+        </Container>
 
+        </>
     )
 }
