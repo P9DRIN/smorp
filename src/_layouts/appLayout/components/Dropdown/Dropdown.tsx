96ea70c0785fb4fa541f92d33c37a0c4d5a47b1b
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 15:20:24 -0300

    added to responsefully screens

--- src/_layouts/appLayout/components/Dropdown/Dropdown.tsx
+++ src/_layouts/appLayout/components/Dropdown/Dropdown.tsx
@@ -4,31 +4,47 @@ import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 import './style.css'
 import { Scroll, SignOut, UserList } from 'phosphor-react'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../../../context/AuthContext'
+
 
 export function DropDown(){
+
+    const { logoutAccount } = useContext(AuthContext)
+   
+    
     return(
         <DropdownMenu.Root>
+
             <DropdownMenu.Trigger asChild>
                 <Button className='classe'><HamburgerMenuIcon width={32} height={32}/></Button>
             </DropdownMenu.Trigger>
+
         <DropdownMenu.Portal>
+
             <DropdownMenu.Content className="dropdownContent">
+
                 <Link to='/profile'>
                 <DropdownMenu.Item className='dropdownItem'>
                     <UserList className='svg'/>Perfil
                 </DropdownMenu.Item>
                 </Link>
+
                 <Link to='/orders'>
                 <DropdownMenu.Item className='dropdownItem'>
                     <Scroll className='svg'/>Pedidos
                 </DropdownMenu.Item>
                 </Link>
-                <Link to='/auth'>
-                <DropdownMenu.Item className="dropdownItem">
+
+                <Link to ='/auth'>
+                <DropdownMenu.Item className="dropdownItem" onClick={logoutAccount}>
                     <SignOut className='svg'/>Logout
                 </DropdownMenu.Item>
                 </Link>
+               
+
             </DropdownMenu.Content>
+
         </DropdownMenu.Portal>
 
         </DropdownMenu.Root>
